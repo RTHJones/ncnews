e760deb7897cc58e401d66f4758fce07e698951a
RTHJones <robin.thedevilhimself.jones@googlemail.com>
2019-05-20 11:23:55 +0100

    added JSON output describing endpoints to the /api GET route

--- routes/api.js
+++ routes/api.js
@@ -11,12 +11,35 @@ apiRouter.use('/users', userRouter);
 apiRouter
   .route('/')
   .get((req, res) => res.send({
-    '/users': 'username',
-    '/articles': 'atricle_id',
-    '/comments': 'comment_id',
-    '/topics/': 'returns all topics'
-  }})
-    .all(methodNotAllowed);
+    'api/users/:username': {
+      'GET': 'returns user details for that username'
+    },
+    'api/articles': {
+      'GET': {
+        '/:article_id': 'returns a specific article',
+        '/': 'returns all articles',
+        '/:article_id/comments': 'returns all comments for a specific article'
+      },
+      'PATCH': {
+        '/articles?inc_votes=1': 'increases vote count on specific article'
+      },
+      'POST': {
+        '/:article_id/comments?username=:username&body=commenttext': 'adds comment body to specific article and attributes it to username'
+      }
+    },
+    'api/comments': {
+      'GET': {
+        '/:comment_id': 'returns a specific comment'
+      },
+      'DELETE': {
+        '/comment_id': 'deletes a specific comment and logs that comment to the console for confirmation'
+      },
+    },
+    'api/topics/': {
+      'GET': 'returns all topics'
+    }
+  }))
+  .all(methodNotAllowed);
 
 
 
